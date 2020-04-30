f6be90a00065396fbb33408462aaa83a3b49d976
nairadarshm <57188873+nairadarshm@users.noreply.github.com>
2020-04-30 12:48:22 +0530

    Update sum_of_digit.c

--- sum_of_digit.c
+++ sum_of_digit.c
@@ -7,10 +7,11 @@ int main()
    scanf("%d",&n);    
    while(n>0)    
       {  static int m; 
-         m=n%10;    
+         m=n%10;   
          sum=sum+m;    
          n=n/10;    
       }    
+   
    printf("Sum is=%d",sum);    
    return 0;  
 }   
